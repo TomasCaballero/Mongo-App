36eb3159fb525dd7c3bd05caa1af17199ac77f6d
Tomas Caballero <caballerotomas97@gmail.com>
2023-11-23 20:20:51 -0300

    Incorporación del server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,29 @@
-import fs from 'fs';
+import net from "node:net";
+import dotenv from "dotenv";
+dotenv.config();
 
-console.log(fs);
+let PORT = process.env.PORT ?? 2323;
+
+const serverTCP = net.createServer();
+
+serverTCP.on("connection", (socket) => {
+
+    socket.on("data", () => {
+
+    });
+
+    socket.on("close", () => {
+        console.log("Client disconnected",new Date().toLocaleString())
+    });
+
+    socket.on("error", () => {
+        console.log("Client error",new Date().toLocaleString())
+    });
+
+
+    console.log("Client connection established", new Date().toLocaleString());
+})
+
+serverTCP.listen(PORT,()=>{
+    console.log(`Server is up on port: ${PORT} - ${new Date().toLocaleString()}`);
+})
