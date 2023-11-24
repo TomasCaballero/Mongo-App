427196c6dba697b110617dba4946e49048796006
Tomas Caballero <caballerotomas97@gmail.com>
2023-11-23 21:34:44 -0300

    Incorporación de registro log

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import net from "node:net";
 import dotenv from "dotenv";
+import { writeHistory } from "../utils/handleHistory.js";
+import { randomUUID } from "node:crypto";
 dotenv.config();
 
 let PORT = process.env.PORT ?? 2323;
@@ -8,6 +10,7 @@ const serverTCP = net.createServer();
 
 serverTCP.on("connection", (socket) => {
 
+    const id = randomUUID();
     socket.on("data", (bufferData) => {
         const data = JSON.parse(bufferData.toString());
 
@@ -15,15 +18,16 @@ serverTCP.on("connection", (socket) => {
     });
 
     socket.on("close", () => {
-        console.log("Client disconnected",new Date().toLocaleString())
+        writeHistory('disconnected', id);
     });
 
     socket.on("error", () => {
-        console.log("Client error",new Date().toLocaleString())
+        writeHistory('disconnected', id);
     });
 
 
     console.log("Client connection established", new Date().toLocaleString());
+    writeHistory('connected', id);
 })
 
 serverTCP.listen(PORT,()=>{
